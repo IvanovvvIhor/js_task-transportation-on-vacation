3aa1668087b5060f06cb324d8ab61c5f7fa37433
Ivanov Ihor <i.ivanovvv21122006@gmail.com>
2026-01-17 20:36:15 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,21 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const rentDay = 40;
+  const LONG_STRIC = 7;
+  const LONG_STRIC_DISCOUNT = 50;
+  const LOW_STRIC = 3;
+  const LOW_STRIC_DISCOUNT = 20;
+
+  let totalCost = 1 * days * rentDay;
+
+  if (days >= LONG_STRIC) {
+    totalCost -= LONG_STRIC_DISCOUNT;
+  } else if (days >= LOW_STRIC) {
+    totalCost -= LOW_STRIC_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
